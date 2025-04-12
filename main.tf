29fb953e3dc63ba61d759bf06d9266eb7a86379b
yashtnaik <yashtnaik@gmail.com>
2025-04-12 23:16:07 +0530

    ig

--- main.tf
+++ main.tf
@@ -30,22 +30,8 @@ resource "aws_vpc_security_group_ingress_rule" "tejas_ipv4" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "tejas_ipv6" {
-  security_group_id = aws_security_group.tejas.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.tejas.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.tejas.id
-  cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
