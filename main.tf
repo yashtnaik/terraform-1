3187cd2d529c6e4a7f5f4b670db55bfadb2bcf53
yashtnaik <yashtnaik@gmail.com>
2025-04-13 00:09:20 +0530

    g

--- main.tf
+++ main.tf
@@ -30,8 +30,7 @@ resource "aws_instance" "terraform_1" {
   ami                     = "ami-002f6e91abff6eb96"
   instance_type           = "t2.micro"
   key_name = var.key
-  aws_security_group = [aws_security_group.tejas.id]
-
+  security_groups = "${aws_security_group.tejas.id}"
   tags = {
     Name = "terraform-1"
   }
