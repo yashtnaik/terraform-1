0524d21dfc472c1ca47837801d4969e7136aea6f
yashtnaik <yashtnaik@gmail.com>
2025-04-12 23:01:11 +0530

    sg

--- main.tf
+++ main.tf
@@ -20,9 +20,16 @@ resource "aws_security_group" "terraform_1" {
     protocol  = -1
     self      = true
     from_port = 0
-    to_port   = 8080
+    to_port   = 0
     }
     tags = {
     Name = "terraform-1"
   }
+}
+resource "aws_security_group_rule" "jenkins" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  security_group_id = aws_security_group.terraform_1.id
 }
