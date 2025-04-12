27540d335cbbfffdb8e15305776ac156d93c56b1
yashtnaik <yashtnaik@gmail.com>
2025-04-12 22:54:08 +0530

    pem

--- main.tf
+++ main.tf
@@ -7,3 +7,22 @@ resource "aws_instance" "terraform_1" {
     Name = "terraform-1"
   }
 }
+
+resource "aws_security_group" "terraform_1" {
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 8080
+    }
+    tags = {
+    Name = "terraform-1"
+  }
+}
