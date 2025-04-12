c5de78e3930c957fe9bc06dd441c4ca751aaf038
yashtnaik <yashtnaik@gmail.com>
2025-04-12 23:10:33 +0530

    tejas

--- main.tf
+++ main.tf
@@ -18,16 +18,16 @@ resource "aws_security_group" "tejas" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+resource "aws_vpc_security_group_ingress_rule" "tejas_ipv4" {
+  security_group_id = aws_security_group.tejas.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+resource "aws_vpc_security_group_ingress_rule" "tejas_ipv6" {
+  security_group_id = aws_security_group.tejas.id
   cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
@@ -35,13 +35,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.tejas.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.tejas.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
