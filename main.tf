3ca887b007d238205291174ad2c3d9d23f921ba4
yashtnaik <yashtnaik@gmail.com>
2025-04-12 19:52:00 +0530

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "terraform-1" {
+resource "aws_instance" "terraform_1" {
   ami                     = "ami-0dee22c13ea7a9a67"
   instance_type           = "t2.micro"
   key_name = var.key
