f2171c6593ae8e0c9dcf9c4a9d8262362206dc4e
yashtnaik <yashtnaik@gmail.com>
2025-04-13 17:20:41 +0530

    sg

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform_1" {
   ami                     = "ami-002f6e91abff6eb96"
   instance_type           = "t2.micro"
   key_name = var.key
-  security_groups = values(aws_security_group.allow_tls.id)
+  security_groups = "${aws_security_group.allow_tls.id}"
   tags = {
     Name = "terraform-1"
   }  
