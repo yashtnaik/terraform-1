bde8d8ba5d8ee116b0f3c5363817fa074a59cd1b
yashtnaik <yashtnaik@gmail.com>
2025-04-13 17:24:12 +0530

    d

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,10 +1,9 @@
-resource "aws_instance" "terraform_1" {
-  ami                     = "ami-002f6e91abff6eb96"
-  instance_type           = "t2.micro"
-  key_name = var.key
-  security_groups = "${output.sg}"
-  tags = {
-    Name = "terraform-1"
-  }  
-  depends_on = [ aws_security_group.allow_tls ]
-}
+# resource "aws_instance" "terraform_1" {
+#   ami                     = "ami-002f6e91abff6eb96"
+#   instance_type           = "t2.micro"
+#   key_name = var.key
+#   security_groups = "${output.sg}"
+#   tags = {
+#     Name = "terraform-1"
+#   }  
+# }
