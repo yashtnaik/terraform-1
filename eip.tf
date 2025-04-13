8cf214613661e095076a334fa830d889eaeb884c
yashtnaik <yashtnaik@gmail.com>
2025-04-13 12:00:45 +0530

    hs

--- eip.tf
+++ eip.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "${aws_eip.tejas.address}/32"
+  cidr_ipv4         = "${aws_eip.tejas.public_ip}/32"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
