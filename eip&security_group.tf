e487b5608bd7fc54ad2e46b69daa1324032361eb
yashtnaik <yashtnaik@gmail.com>
2025-04-14 12:32:20 +0530

    s

--- eip&security_group.tf
+++ eip&security_group.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   tags = {
-    Name = "allow_tls"
+    Name = "tejas"
   }
 }
 
