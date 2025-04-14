ce77642a3790af625852184fba035ed3090bde68
yashtnaik <yashtnaik@gmail.com>
2025-04-14 17:52:49 +0530

    ss

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform_1" {
-  ami                     = output.ami_ids.id
+  ami                     = data.aws_ami.my_ami.id
   instance_type           = "t2.micro"
   key_name = var.key
 #   vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
