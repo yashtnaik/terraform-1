21c0cb288b252e919ec5419b5289834350028687
yashtnaik <yashtnaik@gmail.com>
2025-04-14 12:28:38 +0530

    s

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "terraform_1" {
   ami                     = "ami-002f6e91abff6eb96"
   instance_type           = "t2.micro"
   key_name = var.key
-  vpc_security_group_ids = [output.sg]
+  vpc_security_group_ids = [ output.sg ]
   tags = {
     Name = "terraform-1"
   }  
-  depends_on = [ output.sg ]
+  depends_on = [ aws_security_group.allow_tls ]
 }
