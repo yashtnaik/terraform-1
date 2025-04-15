5a8fbdedf9cb9f21ebd3525ab3f69b5ee5eb8cc4
yashtnaik <yashtnaik@gmail.com>
2025-04-15 12:38:44 +0530

    data

--- data_instance_fetch.tf
+++ data_instance_fetch.tf
@@ -2,7 +2,7 @@
 #     instance_state_names = ["running", "stopped"]
 # }
 
-# data "aws_ami" "my_ami" {
-#     most_recent      = true
-#     owners           = ["amazon"]
-# }
+data "aws_ami" "my_ami" {
+    most_recent      = true
+    owners           = ["amazon"]
+}
