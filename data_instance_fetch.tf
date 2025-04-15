f4bbf14157535fd85f2783f815e4a42c358a90f5
yashtnaik <yashtnaik@gmail.com>
2025-04-15 12:33:34 +0530

    try

--- data_instance_fetch.tf
+++ data_instance_fetch.tf
@@ -1,8 +1,8 @@
-data "aws_instances" "try" {
-    instance_state_names = ["running", "stopped"]
-}
+# data "aws_instances" "try" {
+#     instance_state_names = ["running", "stopped"]
+# }
 
-data "aws_ami" "my_ami" {
-    most_recent      = true
-    owners           = ["amazon"]
-}
+# data "aws_ami" "my_ami" {
+#     most_recent      = true
+#     owners           = ["amazon"]
+# }
--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-# output "eip_public_ip" {
-#     value = aws_eip.tejas.public_ip
+# # output "eip_public_ip" {
+# #     value = aws_eip.tejas.public_ip
+# # }
+
+# # output "sg" {
+# #     value = aws_security_group.allow_tls.id 
+# # }
+
+# output "data_instance" {
+#     value = data.aws_instances.try.ids
 # }
 
-# output "sg" {
-#     value = aws_security_group.allow_tls.id 
+# output "ami_ids" {
+#     value = data.aws_ami.my_ami.id
 # }
-
-output "data_instance" {
-    value = data.aws_instances.try.ids
-}
-
-output "ami_ids" {
-    value = data.aws_ami.my_ami.id
-}
